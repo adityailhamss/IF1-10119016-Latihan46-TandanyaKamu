e75b5815caea4488eaeda199807cfc4f1ba00e9e
aditi <aditi@DESKTOP-343N0FS>
2020-11-14 22:39:38 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan46/tandanyakamu/Program.java
@@ -0,0 +1,33 @@
+package if1.pkg10119016.latihan46.tandanyakamu;
+import java.util.Date;
+import java.util.Scanner;
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan program 
+ * umur menggunakan getter setter
+ */
+public class Program {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Age umur = new Age(2020);
+        
+        System.out.print("Masukkan Tahun Lahir Anda : ");
+        umur.setYearBirth(scanner.nextInt());
+        System.out.println();
+        System.out.println("======Hasil Perhitungan Umur======");
+        int yearBirth = umur.getYearBirth();
+        System.out.println("Tahun Lahir Anda : " + yearBirth);
+        int yearNow = umur.getYearNow();
+        System.out.println("Hari ini Tahun : " + yearNow);
+        int usia = umur.hitungUmur();
+        System.out.println("Umur kamu sampai hari ini adalah " + usia + " tahun");
+        System.out.println("Tandanya Kamu " + umur.tandanyaKamu(usia));
+    }
+    
+}
